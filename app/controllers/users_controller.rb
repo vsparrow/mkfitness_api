e62963eef943f86cd6cacad6efb0880cdadaba30
vsparrow <khan@vsparrow.com>
2018-09-17 23:47:19 -0400

    Setup routes for User, tested JSON response

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  #GET /users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  #POST /users
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  # /GET /users/:id
+  def show
+    json_response(@user)
+  end
+
+  #no update or delete by design
+
+  private
+
+  def user_params
+    #whitelist params
+    params.permit(:firstname, :lastname, :username)
+  end
+
+  def set_users
+    @user = User.find(params[:id])
+  end
+end #UsersController
