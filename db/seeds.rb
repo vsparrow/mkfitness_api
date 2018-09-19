13d60a9e127c053d9ac5e8f22a9371f653a8e80d
vsparrow <khan@vsparrow.com>
2018-09-18 22:43:39 -0400

    seed.rb setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # #!/usr/bin/ruby
-# require 'date'
+require 'date'
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -8,22 +8,93 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!(firstname: "Marty", lastname: "Zuckerberg", username: "mzuckerberg")
-id = user.id
 
-counter = 0
-weight = 300
-waist = 46
-while counter < 120 do
-  date = Time.new(2018,3,5).to_date
-         # Time.new(2001,2,3).to_date
-  # puts `date is #{date}, weight is #{weight}`
-  # puts "weight is #{weight}"
 
-  waist = waist - .1
-  weight = weight - 0.3
-  date += 1
-  counter +=1
+def get_day_of_week(date)
+  date.strftime('%A')
 end
 
-puts "waist : #{waist}, weight: #{weight}, date: #{date}"
+def get_elliptical_minutes(today)
+  minutes = 30
+  if(today == "Saturday" || today == "Sunday")
+    minutes = 0
+  end
+  minutes
+end
+
+def get_elliptical_distance(today)
+  distance = 3 # miles
+  if(today == "Saturday" || today == "Sunday")
+    distance = 0
+  end
+  distance
+end
+
+def get_note(today)
+  note = "Error if I don't change"
+  case today
+    when "Monday"
+      note = "Back Day"
+    when "Tuesday"
+      note = "Legs today!"
+    when "Wednesday"
+      note = "Chest Day"
+    when "Thursday"
+      note = "Bis and Tris"
+    when "Friday"
+      note = "Shoulders"
+    when "Saturday"
+      note = "Day Off"
+    when "Sunday"
+      note = "Day Off"
+  end #case
+  note
+end # get_note
+
+
+def start_seed
+  #user variables
+  user = User.create!(firstname: "Marty", lastname: "Zuckerberg", username: "mzuckerberg")
+  id = user.id
+  #starting variables
+  counter = 0
+  weight = 300
+  waist = 50
+  date = Time.new(2018,5,1).to_date
+  today = "Error in method if not changed"
+  note = ""
+  stop_eat_at_x = true
+  minutes = 30
+  distance = 0
+  ate_sugar = false
+  carbs_time_stopped_eating = "6pm"
+
+  while counter < 120 do
+    today = get_day_of_week(date)
+    note = get_note(today)
+    minutes = get_elliptical_minutes(today)
+    distance = get_elliptical_distance(today)
+
+    # if Friday , cheat meals, eat after 6pm ok
+    today == "Friday" ? stop_eat_at_x = false : stop_eat_at_x = true
+    today == "Friday" ? carbs_time_stopped_eating = "11pm" : carbs_time_stopped_eating = "6pm"
+    today == "Friday" ? ate_sugar = true : ate_sugar = false
+
+    #save to db
+    WaistSize.create!( inches: waist.ceil, user_id: id, date: date)
+    Lift.create!(notes: note, user_id: id, date: date)
+    Weight.create!(weight: weight.ceil, user_id: id, date: date)
+    Carb.create!(stop_eat_at_x: stop_eat_at_x, time: carbs_time_stopped_eating, user_id: id, date: date)
+    Elliptical.create!(minutes: minutes, distance: distance, user_id: id, date: date)
+    Sugar.create!(ate_sugar: ate_sugar, user_id: id, date: date)
+
+    #changes for the next iteration of loop
+    waist = waist - 0.07
+    weight = weight - 0.3
+    date += 1
+    counter +=1
+    # puts "waist : #{waist.ceil}, weight: #{weight.ceil}, date: #{date}"
+  end
+end
+
+start_seed
--- temp.rb
+++ temp.rb
@@ -5,8 +5,7 @@ def get_day_of_week(date)
   date.strftime('%A')
 end
 
-def get_elliptical_minutes(date)
-  today = get_day_of_week(date)
+def get_elliptical_minutes(today)
   minutes = 30
   if(today == "Saturday" || today == "Sunday")
     minutes = 0
@@ -14,8 +13,7 @@ def get_elliptical_minutes(date)
   minutes
 end
 
-def get_elliptical_distance(date)
-  today = get_day_of_week(date)
+def get_elliptical_distance(today)
   distance = 3 # miles
   if(today == "Saturday" || today == "Sunday")
     distance = 0
@@ -23,9 +21,7 @@ def get_elliptical_distance(date)
   distance
 end
 
-def get_note(date)
-  # puts "Date is #{date}, #{date.strftime('%A')}" #get day of week
-  today = get_day_of_week(date)
+def get_note(today)
   note = "Error if I don't change"
   case today
     when "Monday"
@@ -47,34 +43,38 @@ def get_note(date)
 end # get_note
 
 
-counter = 0
-weight = 300
-waist = 50
-date = Time.new(2018,3,5).to_date
-note = ""
-stop_eat_at_x = true
-minutes = 30
-distance = 0
-ate_sugar = false
-carbs_time_stopped_eating = "6pm"
-while counter < 20 do
+def start_seed
+  counter = 0
+  weight = 300
+  waist = 50
+  date = Time.new(2018,5,1).to_date
+  today = "Error in method if not changed"
+  note = ""
+  stop_eat_at_x = true
+  minutes = 30
+  distance = 0
+  ate_sugar = false
+  carbs_time_stopped_eating = "6pm"
 
-         # Time.new(2001,2,3).to_date
-  # puts `date is #{date}, weight is #{weight}`
-  # puts "weight is #{weight}"
-  note = get_note(date)
-  # puts note
-  minutes = get_elliptical_minutes(date)
-  distance = get_elliptical_distance(date)
-  puts " today is #{get_day_of_week(date)} and distance is #{distance} and minutes is #{minutes}"
-  # if Friday , cheat meals, eat after 6pm ok
-  get_day_of_week(date) == "Friday" ? stop_eat_at_x = false : stop_eat_at_x = true
-  get_day_of_week(date) == "Friday" ? carbs_time_stopped_eating = "11pm" : carbs_time_stopped_eating = "6pm"
-  get_day_of_week(date) == "Friday" ? ate_sugar = true : ate_sugar = false
-  waist = waist - 0.05
-  weight = weight - 0.3
-  date += 1
-  counter +=1
+  while counter < 120 do
+    today = get_day_of_week(date)
+    note = get_note(today)
+    minutes = get_elliptical_minutes(today)
+    distance = get_elliptical_distance(today)
+    # puts " today is #{today} and distance is #{distance} and minutes is #{minutes}"
+
+    # if Friday , cheat meals, eat after 6pm ok
+    today == "Friday" ? stop_eat_at_x = false : stop_eat_at_x = true
+    today == "Friday" ? carbs_time_stopped_eating = "11pm" : carbs_time_stopped_eating = "6pm"
+    today == "Friday" ? ate_sugar = true : ate_sugar = false
+
+    #changes for the next iteration of loop
+    waist = waist - 0.07
+    weight = weight - 0.3
+    date += 1
+    counter +=1
+    puts "waist : #{waist.ceil}, weight: #{weight.ceil}, date: #{date}"
+  end
 end
 
-puts "waist : #{waist}, weight: #{weight}, date: #{date}"
+start_seed
